b30c3a8af6d36a1ccd36bf44e46ad6a8b26d0e53
Xiaoming Sun <rsun2014@gmail.com>
2018-08-04 15:17:31 -0700

    Model to json property

--- elasticsearch-springboot/src/main/java/pers/xiaoming/elasticsearch_springboot/model/Blog.java
+++ elasticsearch-springboot/src/main/java/pers/xiaoming/elasticsearch_springboot/model/Blog.java
@@ -1,5 +1,6 @@
 package pers.xiaoming.elasticsearch_springboot.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +11,18 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Blog {
+    @JsonProperty
     private int id;
+
+    @JsonProperty
     private String author;
+
+    @JsonProperty
     private String title;
+
+    @JsonProperty
     private String content;
+
+    @JsonProperty("created_at")
     private Date createdAt;
 }
