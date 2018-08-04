ed3435d90d5fce4a44e371fc4615d6dd90768f89
Xiaoming Sun <rsun2014@gmail.com>
2018-08-04 14:18:29 -0700

    Blog controller

---
+++ elasticsearch-springboot/src/main/java/pers/xiaoming/elasticsearch_springboot/controller/BlogContoller.java
@@ -0,0 +1,33 @@
+package pers.xiaoming.elasticsearch_springboot.controller;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pers.xiaoming.elasticsearch_springboot.model.MyBlog;
+import pers.xiaoming.elasticsearch_springboot.service.BlogService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/blog")
+public class BlogContoller {
+
+    private BlogService service;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public MyBlog post(MyBlog blog) {
+        service.createBlog(blog);
+        return blog;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public MyBlog get(@Param("id") int id) {
+        return service.getBlog(id);
+    }
+
+    @RequestMapping(value = "/range", method = RequestMethod.GET)
+    public List<MyBlog> getByRange(@Param("start") int start, @Param("end") int end) {
+        return service.getBlogs(start, end);
+    }
+}
