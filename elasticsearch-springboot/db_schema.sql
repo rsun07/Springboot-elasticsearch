71b4168e29dc407085f25c664975cd18aed742a1
Xiaoming Sun <rsun2014@gmail.com>
2018-08-04 21:05:12 -0700

    update db schema

--- elasticsearch-springboot/db_schema.sql
+++ elasticsearch-springboot/db_schema.sql
@@ -1,10 +1,8 @@
 CREATE TABLE `blog` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `author` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` text(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `index_blog_title` (`title`)
-  KEY `index_author_name` (`author`),
+  PRIMARY KEY (`title`),
+  KEY `index_author_name` (`author`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
