f443935a86b9ed49045e2fff32fb79b37ef4098b
Xiaoming Sun <rsun2014@gmail.com>
2018-08-07 15:25:10 -0700

    Make it more springboot by remove unnecessary configs, beans

--- springboot-web-service/ssm-springboot/src/main/java/pers/xiaoming/springboot/ssm/Application.java
+++ springboot-web-service/ssm-springboot/src/main/java/pers/xiaoming/springboot/ssm/Application.java
@@ -1,15 +1,13 @@
 package pers.xiaoming.springboot.ssm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.PropertySource;
 
-// @Configuration
-// @ComponentScan("pers.xiaoming.springboot.ssm")
-@PropertySource(value="classpath:jdbc.properties")
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+@MapperScan("pers.xiaoming.springboot.ssm.dao")
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
