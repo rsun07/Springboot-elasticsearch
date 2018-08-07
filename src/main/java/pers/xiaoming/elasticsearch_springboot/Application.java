221eca448b58501fe6db649567422b9200ddfbb0
Xiaoming Sun <rsun2014@gmail.com>
2018-08-06 18:19:12 -0700

    Comment for SpringBootApplication annotation

--- src/main/java/pers/xiaoming/elasticsearch_springboot/Application.java
+++ src/main/java/pers/xiaoming/elasticsearch_springboot/Application.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 @MapperScan("pers.xiaoming.elasticsearch_springboot.dao")
 public class Application {
     public static void main(String[] args) {
