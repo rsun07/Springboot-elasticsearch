5acfc5763338ee3b4f4d7e67a2c037c79ab68724
Xiaoming Sun <rsun2014@gmail.com>
2018-08-07 16:56:14 -0700

    introduce springboot web service client module

---
+++ springboot-web-service/ssm-springboot-client/src/main/java/pers/xiaoming/springboot/ssm/controller/StudentClientController.java
@@ -0,0 +1,10 @@
+package pers.xiaoming.springboot.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentClientController {
+
+}
